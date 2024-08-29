88b0b911fe7ca462e92843c3bede8492e5b7adae
Niklas Fulle <36989748+Salkin263@users.noreply.github.com>
2024-08-29 22:27:08 +0200

    added more functions

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -2,13 +2,11 @@ import Input from "@/components/Input";
 import axios from "axios";
 import { useCallback, useState } from "react";
 import { signIn } from "next-auth/react";
-import { useRouter } from "next/router";
 
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/Fa";
 
 const Auth = () => {
-  const router = useRouter();
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -26,10 +24,8 @@ const Auth = () => {
       await signIn("credentials", {
         email,
         password,
-        redirect: false,
-        callbackUrl: "/",
+        callbackUrl: "/profiles",
       });
-      router.push("/");
     } catch (error) {
       console.log(error);
     }
@@ -97,13 +93,13 @@ const Auth = () => {
               </button>
               <div className="flex flex-row items-center gap-4 mt-8 justify-center">
                 <div
-                  onClick={() => signIn("google", { callbackUrl: "/" })}
+                  onClick={() => signIn("google", { callbackUrl: "/profiles" })}
                   className="w-40 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
                 >
                   <FcGoogle size={30} />
                 </div>
                 <div
-                  onClick={() => signIn("github", { callbackUrl: "/" })}
+                  onClick={() => signIn("github", { callbackUrl: "/profiles" })}
                   className="w-40 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
                 >
                   <FaGithub size={30} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,29 @@
+import { NextPageContext } from "next";
+import { getSession } from "next-auth/react";
+
+import Navbar from "@/components/Navbar";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
+
 export default function Home() {
   return (
     <>
-      <h1 className="">Netflix</h1>
+      <Navbar />
     </>
   );
 }
